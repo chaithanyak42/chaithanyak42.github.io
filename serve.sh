d4c7e602ac3b34a89ef546b9a1d66a2c1b3fd32b
chaithanyak42 <chaithanya.kumar42a@gmail.com>
2025-06-08 18:24:48 +0530

    Revert "Refactor site configuration and layout: switch to custom theme, update social links, and enhance footer and header accessibility. Implement theme toggle functionality and pagination for posts."
    This reverts commit 22da73772b8861edf5d97ca957c9b3ecb8afbd39.

--- serve.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Installing dependencies..."
-bundle install
-
-echo "Building and serving the site..."
-bundle exec jekyll serve --livereload --drafts
-
-echo "Site will be available at http://localhost:4000" 
